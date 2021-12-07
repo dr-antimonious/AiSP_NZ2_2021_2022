6e4b7988ee2acddab34b517fe06173b1e17469c7
Leo Tumbas <ltumbas@etfos.hr>
2021-12-07 13:57:34 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace AiSP_NZ2_2021_2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            input = Console.ReadLine();
+
+            char[] data = new char[input.Length];
+            int i = 0;
+            foreach (char c in input)
+            {
+                data[i] = c;
+                i++;
+            }
+            foreach(char s in data) Console.WriteLine(s);
+
+
+        }
+    }
+}
